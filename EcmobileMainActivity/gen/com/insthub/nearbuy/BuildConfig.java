c432b70e2cd092700004e07c1ab1d0269bbc5c37
fs1989 <farseer1989@outlook.com>
2014-08-12 08:15:34 +0800

    第一次上传
    原始版本

---
+++ EcmobileMainActivity/gen/com/insthub/nearbuy/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.insthub.nearbuy;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ EcmobileMainActivity/src/com/insthub/nearbuy/activity/G2_InfoActivity.java
@@ -0,0 +1,73 @@
+package com.insthub.nearbuy.activity;
+//
+//                       __
+//                      /\ \   _
+//    ____    ____   ___\ \ \_/ \           _____    ___     ___
+//   / _  \  / __ \ / __ \ \    <     __   /\__  \  / __ \  / __ \
+//  /\ \_\ \/\  __//\  __/\ \ \\ \   /\_\  \/_/  / /\ \_\ \/\ \_\ \
+//  \ \____ \ \____\ \____\\ \_\\_\  \/_/   /\____\\ \____/\ \____/
+//   \/____\ \/____/\/____/ \/_//_/         \/____/ \/___/  \/___/
+//     /\____/
+//     \/___/
+//
+//  Powered by BeeFramework
+//
+
+import com.insthub.BeeFramework.model.BusinessResponse;
+import org.json.JSONObject;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.external.androidquery.callback.AjaxStatus;
+import com.insthub.BeeFramework.activity.BaseActivity;
+import com.insthub.nearbuy.R;
+import com.insthub.nearbuy.adapter.G2_InfoAdapter;
+import com.insthub.nearbuy.model.HelpModel;
+import com.insthub.nearbuy.model.ProtocolConst;
+import com.insthub.nearbuy.protocol.ApiInterface;
+
+public class G2_InfoActivity extends BaseActivity implements BusinessResponse
+{
+    HelpModel dataModel;
+    ListView helpListView;
+    G2_InfoAdapter listAdapter;
+    private TextView title;
+    private ImageView back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.g2_info);
+
+        helpListView = (ListView)findViewById(R.id.help_listview);
+        dataModel = new HelpModel(this);
+        dataModel.addResponseListener(this);
+        dataModel.fetchShopHelp();
+        listAdapter = new G2_InfoAdapter(this,dataModel);
+        helpListView.setAdapter(listAdapter);
+
+        back = (ImageView) findViewById(R.id.top_view_back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {                
+                finish();
+            }
+        });
+
+        title = (TextView) findViewById(R.id.top_view_text);
+        title.setText(R.string.help);
+
+    }
+    @Override
+    public void OnMessageResponse(String url, JSONObject jo, AjaxStatus status)
+    {
+        if(url.endsWith(ApiInterface.SHOPHELP))
+        {
+            listAdapter.notifyDataSetChanged();
+        }
+    }
+}
